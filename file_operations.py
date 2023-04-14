dff88f38c085db7838d9deed654f586e3ab6306a
nuongwx <27423086+nuongwx@users.noreply.github.com>
2023-04-15 02:31:59 +0700

    float weight ! int

--- file_operations.py
+++ file_operations.py
@@ -3,7 +3,7 @@ def read_file(filename):
         return (
             int(f.readline()),
             int(f.readline()),
-            [int(x) for x in f.readline().split(",")],
+            [float(x) for x in f.readline().split(",")],
             [int(x) for x in f.readline().split(",")],
             [int(x) for x in f.readline().split(",")],
         )
--- generator.py
+++ generator.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Node:
     def __init__(self, weight, value, class_type, ans=0):
         self.weight = weight
@@ -22,12 +23,19 @@ def generate_dataset(size, num_of_class):
     arr = []
     min_weight = max_weight = 0
     for i in range(num_of_class):
-        arr.append(Node(random.randint(1, 100), random.randint(1, 100), i + 1, 1))
+        arr.append(
+            Node(
+                random.random() * random.randint(1, 100),
+                random.randint(1, 100),
+                i + 1,
+                1,
+            )
+        )
         min_weight += arr[i].weight
     for i in range(size - num_of_class):
         arr.append(
             Node(
-                random.randint(1, 100),
+                random.random() * random.randint(1, 100),
                 random.randint(1, 100),
                 random.randint(1, num_of_class),
             )
@@ -36,14 +44,14 @@ def generate_dataset(size, num_of_class):
     max_weight = sum([i.weight for i in arr])
     random.shuffle(arr)
     return (
-        random.randint(min_weight, max_weight),
+        random.randint(int(min_weight), int(max_weight)),
         num_of_class,
         arr,
     )
 
 
-def print_dataset(capacity, num_of_class, dataset):
-    with open("dataset.txt", "w") as file:
+def print_dataset(capacity, num_of_class, dataset, filename="dataset.txt"):
+    with open(filename, "w") as file:
         file.write(str(capacity) + "\n")
         file.write(str(num_of_class) + "\n")
         file.write(", ".join([str(i.weight) for i in dataset]) + "\n")
@@ -81,7 +89,7 @@ def verify_answer():
 
 def main():
     capacity, num_of_class, dataset = generate_dataset(20, 5)
-    print_dataset(capacity, num_of_class, dataset)
+    print_dataset(capacity, num_of_class, dataset, "INPUT_" + str(capacity) + ".txt")
     # print(verify_answer())
 
 
